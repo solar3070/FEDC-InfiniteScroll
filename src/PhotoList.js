5a957d42f31f32372cd21912cdad2a74f1ba1fe1
HyeJun <solar3070@naver.com>
2022-11-21 17:25:02 +0900

    feat: 더보기 버튼 클릭으로 다음 이미지 불러오기

--- src/PhotoList.js
+++ src/PhotoList.js
@@ -7,7 +7,7 @@
  * ]
  */
 export default function PhotoList({ $target, initialState, onScrollEnded }) {
-  const $photoList = document.createElement("ul");
+  const $photoList = document.createElement("div");
   $target.appendChild($photoList);
 
   this.state = initialState;
@@ -19,16 +19,25 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
 
   this.render = () => {
     $photoList.innerHTML = `
-      ${this.state
-        .map(
-          (photo) =>
-            `<li>
-          <img width="100%" src="${photo.imagePath}" />
-        </li>`
-        )
-        .join("")}
+      <ul>
+        ${this.state
+          .map(
+            (photo) =>
+              `<li style="list-style: none;">
+            <img width="100%" src="${photo.imagePath}" />
+          </li>`
+          )
+          .join("")}
+      </ul>
+      <button class="PhotoList__loadMore" style="width: 100%; height: 200px; font-size: 20px;">Load More</button>
     `;
   };
 
   this.render();
+
+  $photoList.addEventListener("click", (e) => {
+    if (e.target.className === "PhotoList__loadMore") {
+      onScrollEnded();
+    }
+  });
 }
